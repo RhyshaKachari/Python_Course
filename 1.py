2a73e98dad88021927ed104a5a0ad882250bf6e9
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-04-21 10:26:52 +0530

    print command

---
+++ 1.py
@@ -0,0 +1,6 @@
+print('heallo')
+print("hello world")
+print("'hello world'")
+print('"hello world"')
+
+# you can use single quotes inside double quotes and double quotes inside single quotes but you cannout use single quotes inside single quotes and double quotes inside double quotes
