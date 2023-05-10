9b21b89ddc783d5e94eee033499ea3e5b24b08b1
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-05-10 12:21:02 +0530

    Python as a calculator

---
+++ 5.py
@@ -0,0 +1,21 @@
+#Raw strings
+print(r"this is \n")
+
+#python as a calculator
+print(2+3*4)
+print(2/4) #floating point division
+print(4/2)
+print(4//2) #integer division
+print(2//4)
+print(2**3)
+print(2**0.5)
+print(round(2**0.5,4))
+print(3%2) #gives remainder
+
+
+#PRECEDENCE RULE
+# OPERATORS   | PRECEDENCE AND ASSOCIATIVITY RULE
+# PARENTHESE  | HIGHEST
+# EXPONENT    | RIGHT TO LEFT
+# *,/,//,%    | LEFT TO RIGHT
+# +,-         | LEFT TO RIGHT
