b25184bc513587caad67060f64e9ca43286a6826
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-05-10 12:02:02 +0530

    Chapter 1 - Exercise 1

---
+++ 4.py
@@ -0,0 +1,10 @@
+# ********print these following lines*********
+# this is \\ double backslash
+# these are /\/\/\/\/\ mountains
+# he is    awesome (use escape sequence instead of manual spaces)
+# \" \n \t \' (print this as an output)
+
+print("this is \\\\ double backlash")
+print ("/\\/\\/\\/\\")
+print("he is \t awesome ")
+print("\\\" \\n \\t \\")
