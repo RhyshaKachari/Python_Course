f8a95ca8b77b30c14963c27c582cb65e4441b215
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-05-10 11:40:14 +0530

    Escape Sequences

---
+++ 2.py
@@ -0,0 +1,14 @@
+#Escape sequences
+
+# \' - single quote
+# \" - double quote
+# \\ - backslash
+# \n - newline
+# \t - tab
+# \ b - backspace
+
+print("hello \"world \" world")
+print('I\'m harshit')
+print("line A\nline B \n line C")
+print("My name is \t Rhysha")
+print("helll\bo")
